9eadef60a7994b4c1e427e5aa9541f687715f10f
김동룡 <kbg0407@naver.com>
2026-09-09 17:35:31 +0900

    [Passed] 이상한 수열 (47ms, 16MB)

---
+++ trail2/이상한 수열/a-strange-sequence.py
@@ -0,0 +1,13 @@
+N = int(input())
+
+# Please write your code here.
+
+def strange(N):
+    if N == 1:
+        return 1
+    elif N == 2:
+        return 2
+    else:
+        return strange(N//3) + strange(N-1)
+
+print(strange(N))
