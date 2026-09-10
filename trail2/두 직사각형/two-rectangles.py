e1fb4341adf6cc83eba682ff2cb42b338bc4c869
김동룡 <kbg0407@naver.com>
2026-09-10 17:48:00 +0900

    [Passed] 두 직사각형 (49ms, 16MB)

---
+++ trail2/두 직사각형/two-rectangles.py
@@ -0,0 +1,10 @@
+x1, y1, x2, y2 = map(int, input().split())
+a1, b1, a2, b2 = map(int, input().split())
+
+# Please write your code here.
+def square(x1, y1, x2, y2, a1, b1, a2, b2):
+    if x2 < a1 or a2 <x1 or y2 < b1 or b2 < y1:
+        return "nonoverlapping"
+    return "overlapping"
+
+print(square(x1, y1, x2, y2, a1, b1, a2, b2))
