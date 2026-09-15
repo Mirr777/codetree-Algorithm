2160eab716f3f9da3ebe58e18af72246753a8457
김동룡 <kbg0407@naver.com>
2026-09-15 22:55:05 +0900

    [Passed] 두 점으로 만드는 정사각형 (53ms, 16MB)

---
+++ trail2/두 점으로 만드는 정사각형/square-made-of-two-points.py
@@ -0,0 +1,7 @@
+x1, y1, x2, y2 = map(int, input().split())
+a1, b1, a2, b2 = map(int, input().split())
+
+# Please write your code here.
+a ,b,c,d = min(x1,a1), min(y1,b1), max(x2,a2), max(y2,b2)
+e = max(c-a, d-b)
+print(e**2)
